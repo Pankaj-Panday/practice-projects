1dfcc044c0f29e0bcbe7c95bab71e3192c0c0fe7
Pankaj Panday <ppanday064@gmail.com>
2024-04-01 18:00:14 +0530

    redux day 1

---
+++ redux/01-redux-tut/src/features/counter/Counter.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import css from "./counter.module.css";
+import { useSelector, useDispatch } from "react-redux";
+import {
+	increment,
+	decrement,
+	incrementByAmount,
+	incrementAsync,
+	selectCount,
+} from "./counterSlice.js";
+
+const Counter = () => {
+	const count = useSelector(selectCount);
+	const dispatch = useDispatch();
+	const [incrementAmount, setIncrementAmount] = useState("2");
+	return (
+		<div>
+			<div className={css.row}>
+				<button
+					className={css.button}
+					onClick={() => {
+						dispatch(increment());
+					}}
+				>
+					+
+				</button>
+				<span className={css.value}>{count}</span>
+				<button
+					className={css.button}
+					onClick={() => {
+						dispatch(decrement());
+					}}
+				>
+					-
+				</button>
+			</div>
+			<div className={css.row}>
+				<input
+					className={css.textbox}
+					value={incrementAmount}
+					onChange={(event) => {
+						setIncrementAmount(event.target.value);
+					}}
+				/>
+				<button
+					className={css.button}
+					onClick={() => {
+						dispatch(incrementByAmount(Number(incrementAmount)));
+					}}
+				>
+					Add Amount
+				</button>
+				<button
+					className={css.asyncButton}
+					onClick={() => {
+						dispatch(incrementAsync(Number(incrementAmount)));
+					}}
+				>
+					Add Async
+				</button>
+			</div>
+		</div>
+	);
+};
+
+export default Counter;
