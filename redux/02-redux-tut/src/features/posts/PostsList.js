3c3aee7fdd17110d0d24397855e0ab7b31335143
Pankaj Panday <ppanday064@gmail.com>
2024-04-03 18:00:33 +0530

    redux day 3

--- redux/02-redux-tut/src/features/posts/PostsList.js
+++ redux/02-redux-tut/src/features/posts/PostsList.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+import PostAuthor from './PostAuthor.js'
+import TimeAgo from './TimeAgo.js'
+import ReactionButtons from './ReactionButtons.js'
+import { selectAllPosts } from './postsSlice.js'
 
 const PostsList = () => {
-  const posts = useSelector((state) => state.posts)
-  const renderedPosts = posts.map((post) => {
+  const posts = useSelector(selectAllPosts)
+  const orderedPosts = posts
+    .slice()
+    .sort((a, b) => b.date.localeCompare(a.date))
+  const renderedPosts = orderedPosts.map((post) => {
     return (
       <article className="post-excerpt" key={post.id}>
         <h3>{post.title}</h3>
         <p className="post-content">{post.content.substring(0, 100)}</p>
+        <TimeAgo timestamp={post.date} />
+        <PostAuthor userId={post.user} />
+        <ReactionButtons post={post} />
         <Link to={`/posts/${post.id}`} className="button muted-button">
           View Post
         </Link>
--- redux/02-redux-tut/src/features/posts/SinglePostPage.js
+++ redux/02-redux-tut/src/features/posts/SinglePostPage.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+import PostAuthor from './PostAuthor'
+import TimeAgo from './TimeAgo'
+import ReactionButtons from './ReactionButtons'
+import { selectPostById } from './postsSlice'
 
 const SinglePostPage = ({ match }) => {
   const { postId } = match.params
-  const post = useSelector((state) =>
-    state.posts.find((post) => post.id === postId),
-  )
+  const post = useSelector((state) => selectPostById(state, postId))
 
   if (!post) {
     return (
@@ -21,6 +23,9 @@ const SinglePostPage = ({ match }) => {
       <article className="post">
         <h2>{post.title}</h2>
         <p className="post-content">{post.content}</p>
+        <PostAuthor userId={post.user} />
+        <TimeAgo timestamp={post.date} />
+        <ReactionButtons post={post} />
         <Link to={`/editPost/${post.id}`} className="button">
           Edit Post
         </Link>
