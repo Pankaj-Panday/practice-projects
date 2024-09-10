d1323b13b014e435b65f9bee8dc329c0f82d5ee0
Pankaj Panday <ppanday064@gmail.com>
2024-09-11 00:50:39 +0530

    Initial commit

---
+++ framer-motion/src/App.jsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import BasicsOfMotion from "./components/BasicsOfMotion";
+
+function App() {
+	return <BasicsOfMotion />;
+}
+
+export default App;
---
+++ framer-motion/src/components/BasicsOfMotion.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BasicsOfMotion = () => {
+	return (
+		<div className="grid place-content-center h-screen gap-[0.8rem]">
+			<div className="w-40 h-40 bg-black"></div>
+		</div>
+	);
+};
+
+export default BasicsOfMotion;
